552854b4ce88118edf0ef2eef634fd438b9e8bb3
yayou <yanes75@hotmail.fr>
2022-09-24 00:36:32 +0200

    added(script.js):function productList & loop on array

--- front/js/script.js
+++ front/js/script.js
@@ -1,33 +1,54 @@
 fetch("http://localhost:3000/api/products/")
-    .then((res) => res.json())
-    .then((data) => {
-        console.log(data)
-        return addProducts(data)
+    .then((response) => {
+        return response.json();
     })
+    .then((data) => {
+        // console.log(data)
+        return productList(data); //homeProduct
+    });
 
-function addProducts(data) {
-    const id = data[0]._id
-    const anchor = createAnchor(id)
-    appendChildrens(anchor)
+
+function productList(array) {
+    //création d'une boucle pour avoir les données pour chaque produit
+    array.forEach((array) => {
+        const id = array._id;
+
+        // variable pour créer la balise alt et son href
+        const aHref = document.createElement("a");
+        aHref.href = "./product.html?id=" + id;
+
+        // variable pour créer la balise article
+        const article = document.createElement("article");
+
+        // donnée et détail pour l'image
+        const image = document.createElement("img");
+        image.src = array.imageUrl;
+        image.alt = array.altTxt + " , " + array.name;
+
+        // donnée et détail dans la balise h3
+        const h3 = document.createElement("h3");
+        h3.classList.add("productName");
+        h3.textContent = array.name;
+
+        // donné et détail dans la balise p
+        const p = document.createElement("p");
+        p.classList.add("productDescription");
+        p.textContent = array.description;
+
+        // appel de la fonction afin de montrer les enfants du parent #Items
+        createTagElement(aHref, article, image, h3, p);
+    });
 }
 
-function createAnchor(id) {
-    const anchor = document.createElement("a")
-    anchor.href = "./product.html?id=" + id
-    return anchor
-}
+//Fonction qui donne les enfants au parent #items
 
-function appendChildrens(anchor) {
-    const items = document.querySelector("#items")
+function createTagElement(aHref, article, image, h3, p) {
+    const items = document.querySelector("#items");
     if (items != null) {
-        items.appendChild(anchor)
+        items.appendChild(aHref);
+        aHref.appendChild(article);
+        article.appendChild(image);
+        article.appendChild(h3);
+        article.appendChild(p);
     }
 }
-
-
-// function createImg(imageUrl, altTxt) {
-//     const image = document.createElement("img")
-//     image.src = imageUrl
-//     image.alt = altTxt
-//     return image
-// }
