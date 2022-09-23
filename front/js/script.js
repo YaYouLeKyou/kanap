c736eb2a39e8ec63cfc63116ce9155f758ede696
yayou <yanes75@hotmail.fr>
2022-09-23 23:15:11 +0200

    added(script.js):function addProduct, createAnchor, appenChildrens

--- front/js/script.js
+++ front/js/script.js
@@ -1,16 +1,33 @@
 fetch("http://localhost:3000/api/products/")
     .then((res) => res.json())
-    .then((data) => addProducts(data))
+    .then((data) => {
+        console.log(data)
+        return addProducts(data)
+    })
 
 function addProducts(data) {
-    //console.log(data)
-    const imageUrl = data[0].imageUrl
+    const id = data[0]._id
+    const anchor = createAnchor(id)
+    appendChildrens(anchor)
+}
 
+function createAnchor(id) {
     const anchor = document.createElement("a")
-    anchor.text = "mes kanap"
-    const items = document.querySelector("items")
+    anchor.href = "./product.html?id=" + id
+    return anchor
+}
+
+function appendChildrens(anchor) {
+    const items = document.querySelector("#items")
     if (items != null) {
         items.appendChild(anchor)
-
     }
 }
+
+
+// function createImg(imageUrl, altTxt) {
+//     const image = document.createElement("img")
+//     image.src = imageUrl
+//     image.alt = altTxt
+//     return image
+// }
